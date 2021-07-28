898c1b52677ee1e9c9f7e4de5f28eda27171ea00
ihateuu <replacement506@gmail.com>
2021-07-28 20:46:57 +0300

    added spacer

--- 06 Assignment Notebook/ContentView.swift
+++ 06 Assignment Notebook/ContentView.swift
@@ -8,9 +8,36 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var assignments =
+        [AssignmentItem(description: "Math Homework", course: "Math", dueDate: Date()),
+         AssignmentItem(description: "Science Homework", course: "Science", dueDate: Date()),
+         AssignmentItem(description: "History Homework", course: "History", dueDate: Date())]
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            List {
+                ForEach(assignments) { assignment in
+                    HStack {
+                        VStack(alignment: .leading) {
+                            Text(assignment.description)
+                                .font(.headline)
+                            Text(assignment.course)
+                        }
+                        Spacer()
+                        Text(assignment.dueDate, style: .date)
+                    }
+                    
+                }
+                .onMove(perform: { indices, newOffset in assignments.move(fromOffsets: indices, toOffset: newOffset)
+                })
+                .onDelete(perform: { indexSet in
+                    assignments.remove(atOffsets: indexSet)
+                })
+                
+            }
+            .navigationBarTitle("Assignment Notebook", displayMode: .inline)
+            .navigationBarItems(leading: EditButton())
+        }
     }
 }
 
@@ -18,4 +45,10 @@ struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
+}
+struct AssignmentItem: Identifiable {
+    var id = UUID()
+    var description = String()
+    var course = String()
+    var dueDate = Date()
 }
