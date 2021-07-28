b3afe01269f425bf79405857cc5f201e23b2f770
ihateuu <replacement506@gmail.com>
2021-07-28 20:02:24 +0300

    Initial Commit

---
+++ 06 Assignment Notebook/_6_Assignment_NotebookApp.swift
@@ -0,0 +1,17 @@
+//
+//  _6_Assignment_NotebookApp.swift
+//  06 Assignment Notebook
+//
+//  Created by Kareem Almomen on 7/28/21.
+//
+
+import SwiftUI
+
+@main
+struct _6_Assignment_NotebookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
